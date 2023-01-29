dfacd36e2dd42397a4e60f28f7397577e9c7e519
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-01-29 17:40:17 +0900

    ..

--- App.vue
+++ App.vue
@@ -8,9 +8,7 @@ import { computed, onMounted, reactive, watchEffect, ref } from 'vue'
 import TabBar from '../components/TabBar.vue'
 import Home from '../pages/Home.vue'
 import Detail from '../pages/Detail.vue'
-import { store } from './plugins/store.js'
 defineProps([])
-const flg = reactive({store.isLogin});
 onMounted(() => {})
 
 const method = () => {}
