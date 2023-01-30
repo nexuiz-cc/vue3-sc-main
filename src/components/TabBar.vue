52145e7de7664a870cb8bba576bde2daf785368a
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-01-30 11:27:29 +0900

    ..

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,7 +1,7 @@
 <template>
   <Tabbar v-model="active" class="tabbar" id="tabbar">
     <TabbarItem icon="home-o" to="/home" name="home">Home</TabbarItem>
-    <TabbarItem icon="search" to="/search" name="search">Search</TabbarItem>
+    <TabbarItem icon="shopping-cart" to="/search" name="search">Product</TabbarItem>
     <TabbarItem icon="friends-o" to="/friends" name="friends">Friends</TabbarItem>
     <TabbarItem icon="setting-o" to="/user" name="user">MyPage</TabbarItem>
   </Tabbar>
