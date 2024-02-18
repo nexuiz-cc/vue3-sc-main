bfb3c07d149ab5236c19bf290131666e1a5969be
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2024-02-18 15:03:34 +0900

    ..

---
+++ src/components/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <Tabbar v-model="active" class="tabbar" id="tabbar">
+      <TabbarItem icon="home-o" to="/home" name="home">DashBoard</TabbarItem>
+      <TabbarItem icon="newspaper-o" to="/menu" name="menu">Menu</TabbarItem>
+      <TabbarItem icon="shopping-cart" to="/sc" name="friends">Shoping Cart</TabbarItem>
+      <TabbarItem icon="setting-o" to="/myPage" name="mypage">MyPage</TabbarItem>
+    </Tabbar>
+  </template>
+
+<script setup>
+import { Tabbar, TabbarItem } from 'vant';
+import { ref, onMounted } from 'vue';
+
+const active = ref('home');
+
+onMounted(() => {
+  const tab = document.getElementById('tabbar');
+  tab.style.display = 'flex';
+});
+</script>
+
+  <style scoped>
+  .tabbar {
+    display: none;
+  }
+  </style>
